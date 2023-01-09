c014ef7e3b7247066e967831e021b8162be15458
zwallace425 <zwallace@jcvi.org>
2023-01-09 14:30:00 -0800

    curation test

--- test_curation.py
+++ test_curation.py
@@ -4,14 +4,9 @@ import sys
 import time
 
 query = sys.argv[1]
-strain = sys.argv[2]
-
-boundaryFile = "Flu_profile_boundaries_Main.txt"
-lookupTable = "Flu_profile_lookupTable_Main.txt"
-profile_dir = "testing"
 
 start = time.time()
-result = Curation(query, strain, boundaryFile, lookupTable, profile_dir).curation_table()
+result = Curation(query).curation_table()
 end = time.time()
 
 print("Time for one sequence:", round(end - start, 3), "seconds")
